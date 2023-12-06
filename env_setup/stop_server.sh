c04a0750429fc7d7c05dab2eebe71f30e180d9a6
try-er <avyarth00@gmail.com>
2023-12-06 21:12:49 +0530

    conditional process closing

--- env_setup/stop_server.sh
+++ env_setup/stop_server.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# this file runs first, so during an initail installation error might happen
 
-# simply stop the application process using pm2
-echo stopping server
-sudo su
-pm2 stop npm 
-exit
+echo "Stopping server"
+
+# Check if the process 'sample_deploy' exists in pm2
+process_exists=$(sudo pm2 list | grep -c "sample_deploy")
+
+if [ $process_exists -gt 0 ]; then
+    # If 'sample_deploy' process exists, stop it
+    echo "Stopping 'sample_deploy' process..."
+    sudo pm2 stop sample_deploy
+else
+    # If 'sample_deploy' process doesn't exist, exit
+    echo "'sample_deploy' process not found. Exiting..."
+fi
